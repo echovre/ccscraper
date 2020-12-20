8556a0750d8977b30ad0df96ef60917e2b625829
echovre <you@example.com>
2020-12-20 15:40:12 -0700

    fix port binding for heroku

--- scrape.js
+++ scrape.js
@@ -186,7 +186,7 @@ app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-var server = app.listen(8888, function () {
+var server = app.listen(process.env.PORT || 8888, function () {
   var host = server.address().address;
   var port = server.address().port;
   console.log("App listening at http://%s:%s", host, port);
